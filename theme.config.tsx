3fced9fe4d96fbffbdf563bc7f8e57788303554f
bibekshhh <bibekrock2019@gmail.com>
2023-12-06 14:48:26 +0545

    [setup]: documentation app for zippybox

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>ZippyBox</span>,
+  project: {
+    link: 'https://github.com/Hyperce-internal-demos/zippybox-docs',
+  },
+  docsRepositoryBase: 'https://github.com/Hyperce-internal-demos/zippybox-docs',
+  footer: {
+    text: 'ZippyBox Docs',
+  },
+}
+
+export default config;
