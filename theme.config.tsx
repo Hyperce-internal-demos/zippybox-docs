0f5e450392e815a8ff41452bc20d48fb3f72393d
Binita Shrestha <binitastha34@gmail.com>
2023-12-08 13:20:52 +0545

    meta tags

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,10 +2,30 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 import Logo from './public/imgs/logo.png'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
+import { useConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
+  head: () => {
+    const { asPath, defaultLocale, locale } = useRouter()
+    const { frontMatter } = useConfig()
+    const url =
+      'https://zippbox-docs.vercel.app' +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
+ 
+    return (
+      <>
+      <title>{frontMatter.title || 'ZippyBox Docs'}</title>
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={frontMatter.title || 'ZippyBox Docs'} />
+        <meta
+          property="og:description"
+          content={frontMatter.description || 'Your go-to resource for comprehensive guides, API documentation, and troubleshooting solutions for seamless navigation and optimization of ZippyBox services.'}
+        />
+      </>
+    )
+  },
   logo: <Image src={Logo} alt="ZippyBox Logo" width={100} height={100}/>,
-  
   docsRepositoryBase: 'https://github.com/Hyperce-internal-demos/zippybox-docs',
   footer: {
     text: 'ZippyBox Docs',
