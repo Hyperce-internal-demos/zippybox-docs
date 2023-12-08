1576d82fc90bd313eed7e2cfa85beda1bf86392c
Binita Shrestha <binitastha34@gmail.com>
2023-12-08 10:54:15 +0545

    fixed image issue and github issue #2

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import Logo from './public/imgs/logo.png'
+import Image from 'next/image'
 
 const config: DocsThemeConfig = {
-  logo: <span>ZippyBox</span>,
-  project: {
-    link: 'https://github.com/Hyperce-internal-demos/zippybox-docs',
-  },
+  logo: <Image src={Logo} alt="ZippyBox Logo" width={100} height={100}/>,
+  
   docsRepositoryBase: 'https://github.com/Hyperce-internal-demos/zippybox-docs',
   footer: {
     text: 'ZippyBox Docs',
+  },
+  editLink: {
+    component: null, 
+  },
+  feedback: {
+    content: null, 
   },
 }
 
